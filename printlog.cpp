61cdbcff3d256ec4e836c354666568bf9e9417c8
nielyd <nielong2014@gmail.com>
2018-02-06 17:22:56 +0800

    打印信息增加时间前缀

---
+++ printlog.cpp
@@ -0,0 +1,22 @@
+#include "printlog.h"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+void printlog(const std::string& str)
+{
+    std::time_t t2 = std::time(nullptr);
+    std::cout << std::put_time(std::localtime(&t2), "%Y-%m-%d %H:%M:%S")
+		<< "  " << str << std::endl;
+}
+
+void test()
+{
+    printlog("hah");
+}
+
+int main(void)
+{
+    test();
+    return 0;
+}
---
+++ printlog.h
@@ -0,0 +1,8 @@
+#include <string>
+
+#ifndef __PRINT_LOG_
+#define __PRINT_LOG_
+
+void printlog(const std::string& str);
+
+#endif __PRINT_LOG_
